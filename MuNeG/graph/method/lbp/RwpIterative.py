4a1e0a34bd736a6afa70471f117b69fea9e8219f
Adrian Popiel <adrian.popiel@pwr.edu.pl>
2015-12-12 15:00:49 +0100

    RwpIterative - stable test

--- MuNeG/graph/method/lbp/RwpIterative.py
+++ MuNeG/graph/method/lbp/RwpIterative.py
@@ -22,10 +22,10 @@ class RwpIterative():
                 for j in xrange(0, layer_number):
                     d_matrix_for_node[j][int(i)-1] = separated_network.degree(node.id)
             csr_d_matrix_for_node = sp.csr_matrix(d_matrix_for_node)
-            self.d.update({node : csr_d_matrix_for_node})
+            self.d.update({node.id : csr_d_matrix_for_node})
 
 
-    def random_walk(self, network, class_mat, number_repetitions, depth):
+    def random_walk(self, network, class_mat, start_layer, number_repetitions, depth):
         self.init_d_matrix(network)
         results=dict()
 
@@ -36,37 +36,49 @@ class RwpIterative():
         for node in unknown_nodes:
             res_node=[]
             for i in xrange(number_repetitions):
-                res_node.append(self.random_walk_recursive(network, node, None, depth, 1))
+                res_node.append(self.random_walk_recursive(network, unknown_nodes, node.id, None, start_layer, depth, 1))
             #print 'Finished for node: '+str(node)+'       Classses: '
             #print res_node
             results[node]=res_node
         return results
 
-    def random_walk_recursive(self, network, current_node, visited, depth, counter):
-        edge=self.draw_connection_at_node(network,current_node)
-
-        #print 'Edge: '+str(edge)
-
-        if (depth<=0 or edge is None): # or current_node==visited
-            #print 'Depth: '+str(depth)+'    Visited already: '+str(current_node==visited)
-            return (None, counter)
+    def random_walk_recursive(self, network, unknown_nodes, current_node, visited, start_layer, depth, counter):
+        decided_layer = self.decide_layer_change(current_node, start_layer)
+        if (decided_layer != start_layer):
+            return self.random_walk_recursive(network, current_node, visited, decided_layer, depth-1, counter+1)
         else:
-            if (edge is not None) and (network.node[edge[1]]['cls']!='unk'):
-                #print 'Reached node with class: '+ str(network.node[edge[1]]['cls'])
-                #print 'Class reached: '+str(network.node[edge[1]]['cls'])
-                return (network.node[edge[1]]['cls'],counter)
+            edge=self.draw_connection_at_node(self.separated_networks[str(start_layer)],current_node)
+
+            #print 'Edge: '+str(edge)
+
+            if (depth<=0 or edge is None): # or current_node==visited
+                #print 'Depth: '+str(depth)+'    Visited already: '+str(current_node==visited)
+                return (None, counter)
             else:
-                #remove comment if renadom walk does not allow retracking
-                #visited=current_node
+                nodes_list = list(network.nodes())
+                new_node = filter(lambda n: n.id == edge[1], nodes_list)[0]
+                if (edge is not None) and (new_node not in unknown_nodes):
+                    #print 'Reached node with class: '+ str(network.node[edge[1]]['cls'])
+                    #print 'Class reached: '+str(network.node[edge[1]]['cls'])
+                    return (new_node.label,counter)
+                else:
+                    #remove comment if renadom walk does not allow retracking
+                    #visited=current_node
 
-                #print 'Recursive sampling. Depth: '+str(depth)
-                #print 'Going to: '+str(edge[1])
-                #print 'recursion'
-                return self.random_walk_recursive(network, edge[1], visited, depth-1, counter+1)
+                    #print 'Recursive sampling. Depth: '+str(depth)
+                    #print 'Going to: '+str(edge[1])
+                    #print 'recursion'
+                    return self.random_walk_recursive(network, edge[1], visited, start_layer, depth-1, counter+1)
 
-    def draw_connection_at_node(self, network, node_id):
+    def draw_connection_at_node(self, network, node):
         result=None
-        neighbs = [edge for edge in nx.edges_iter(network, node_id)]
+        neighbs = [edge for edge in nx.edges_iter(network, node)]
         if len(neighbs)>0:
             result = random.choice(neighbs)
         return result
+
+    def decide_layer_change(self, current_node, start_layer):
+        layer_matrix = self.d[current_node]
+        row = layer_matrix[start_layer - 1, :]
+        index = row.data.argmax()
+        return index + 1
--- MuNeG/tests/graph/method/lbp/TestsRwpIterative.py
+++ MuNeG/tests/graph/method/lbp/TestsRwpIterative.py
@@ -25,11 +25,41 @@ class TestsRwpLBP(unittest.TestCase):
         edgesList = utils.prepareEdgesList(edgesData, nodeList)
         class_mat = utils.prepareTestClassMatWithUnknownNodes()
         default_class_mat = utils.prepareTestClassMat()
-        tools = LBPTools(nodes.__len__(), graph, default_class_mat, 100, 0.1, 0.2)
+        tools = LBPTools(nodes.__len__(), graph, default_class_mat, 100, 0.1, 0.8)
 
+        mockito.when(graph).edges_iter(nodeList[4], data=True)\
+            .thenReturn(utils.generateEdges(10, nodeList, edgesData))\
+            .thenReturn(utils.generateEdges(10, nodeList, edgesData))\
+            .thenReturn(utils.generateEdges(10, nodeList, edgesData))\
+            .thenReturn(utils.generateEdges(10, nodeList, edgesData))\
+            .thenReturn(utils.generateEdges(10, nodeList, edgesData))
         mockito.when(graph).edges(data=True).thenReturn(edgesList)
         mockito.when(graph).nodes().thenReturn(nodes)
         tools.separate_layer(graph, [1, 2], default_class_mat, [4])
         method = RwpIterative(tools.graphs)
-        results = method.random_walk(graph, class_mat, 100, 10)
+        results = method.random_walk(graph, class_mat, 1, 100, 10)
         pass
+
+    def generateEdges(self, nrOfEdges, nodes, edgesData):
+            i = 0
+            for i in range(0, nrOfEdges):
+                if (i == 0):
+                    yield (nodes[0], nodes[2], edgesData[0])
+                elif (i == 1):
+                    yield (nodes[1], nodes[3], edgesData[0])
+                elif (i == 2):
+                    yield (nodes[2], nodes[4], edgesData[0])
+                elif (i == 3):
+                    yield (nodes[0], nodes[1], edgesData[0])
+                elif (i == 4):
+                    yield (nodes[1], nodes[4], edgesData[0])
+                elif (i == 5):
+                    yield (nodes[0], nodes[2], edgesData[1])
+                elif (i == 6):
+                    yield (nodes[1], nodes[3], edgesData[1])
+                elif (i == 7):
+                    yield (nodes[1], nodes[4], edgesData[1])
+                elif (i == 8):
+                    yield (nodes[0], nodes[1], edgesData[1])
+                elif (i == 9):
+                    yield (nodes[2], nodes[3], edgesData[1])
