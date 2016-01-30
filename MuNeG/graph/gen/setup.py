7aec0f39d72accd60b9ed38f791e387d2dab9a92
Adrian <adrian.popiel@pwr.edu.pl>
2016-01-30 14:05:19 +0100

    Tesc commit from pycharm

--- MuNeG/graph/gen/setup.py
+++ MuNeG/graph/gen/setup.py
@@ -1,4 +1,9 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
