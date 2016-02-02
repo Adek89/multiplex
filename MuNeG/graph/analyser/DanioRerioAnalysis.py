3987750a786aa18f07ed12255d74f9185bbbc254
Adrian <adrian.popiel@pwr.edu.pl>
2016-02-02 19:52:54 +0100

    Statistical analysis module - db connected

--- MuNeG/graph/analyser/DanioRerioAnalysis.py
+++ MuNeG/graph/analyser/DanioRerioAnalysis.py
@@ -1,7 +1,11 @@
 import graph.analyser.StatisticalAnalysis as sa
 
 def connect_to_danio_rerio():
-    sa.connect_to_data("156.17.131.228", "DanioRerio", "apopiel", "alamakota123")
+    return sa.connect_to_data("156.17.131.228", "DanioRerio", "apopiel", "alamakota123")
 
 if __name__ == "__main__":
-    connect_to_danio_rerio()
+    cursor = connect_to_danio_rerio()
+    cursor.execute('select rwc_iter from DanioRerio.daniorerio.results')
+    results = cursor.fetchall()
+    data = sorted(results.ado_results[0])
+    sa.draw_histogram(data)
--- MuNeG/graph/analyser/StatisticalAnalysis.py
+++ MuNeG/graph/analyser/StatisticalAnalysis.py
@@ -1,6 +1,17 @@
 import adodbapi as ado
+import numpy as np
+import pylab as pl
+import scipy.stats as stats
+
 
 def connect_to_data(host, database, user, password):
-    conn_str = """Provider=SQLOLEDB.1; User ID=%(user)s; Password=%(password)s;"Initial Catalog=(database)s;DataSource=%(host)s"""
-    my_conn = ado.connect(conn_str, user, password, database, host)
+    conn_str = """Provider=SQLOLEDB.1; User ID=%(user)s; Password=%(password)s;"Database=%(database)s;Data Source=%(host)s"""
+    my_conn = ado.connect(conn_str, user, password, host, database)
+    curs = my_conn.cursor()
+    return curs
 
+def draw_histogram(data):
+    fit = stats.norm.pdf(data, np.mean(data), np.std(data))
+    pl.plot(data,fit,'-o')
+    pl.hist(data, normed=True)
+    pl.show()
