12f63ee1fd4ff79a9cf18a023c3bfde39ceb0b27
Adrian <adrian.popiel@pwr.edu.pl>
2016-02-03 19:05:47 +0100

    DanioRerio - basic statistical analysis

--- MuNeG/graph/analyser/DanioRerioAnalysis.py
+++ MuNeG/graph/analyser/DanioRerioAnalysis.py
@@ -3,9 +3,43 @@ import graph.analyser.StatisticalAnalysis as sa
 def connect_to_danio_rerio():
     return sa.connect_to_data("156.17.131.228", "DanioRerio", "apopiel", "alamakota123")
 
+
+def draw_data():
+    data_array = {'reduction': reduction_data, 'rwc_iter': rwc_iter_data}
+    sa.draw_plot(data_array)
+
+    data_array = {'reduction': reduction_data, 'fusion_sum' : fusion_sum_data}
+    sa.draw_plot(data_array)
+
+    data_array = {'reduction': reduction_data, 'fusion_mean' : fusion_mean_data}
+    sa.draw_plot(data_array)
+
+    data_array = {'reduction': reduction_data, 'rwc_sum' : rwc_sum_data}
+    sa.draw_plot(data_array)
+
+    data_array = {'reduction': reduction_data, 'rwc_mean': rwc_mean_data}
+    sa.draw_plot(data_array)
+
+    data_array = {'reduction': reduction_data, 'rwc_last' : rwc_last_data}
+    sa.draw_plot(data_array)
+
+
+def perform_statistical_analysis():
+    sa.compare_distributions(reduction_data, rwc_iter_data)
+
+
 if __name__ == "__main__":
     cursor = connect_to_danio_rerio()
-    cursor.execute('select rwc_iter from DanioRerio.daniorerio.results')
+    cursor.execute('select reduction, rwc_iter, fusion_sum, fusion_mean, rwc_sum, rwc_mean, rwc_last from DanioRerio.daniorerio.results')
     results = cursor.fetchall()
-    data = sorted(results.ado_results[0])
-    sa.draw_histogram(data)
+    reduction_data = sorted(results.ado_results[0])
+    rwc_iter_data = sorted(results.ado_results[1])
+    fusion_sum_data = sorted(results.ado_results[2])
+    fusion_mean_data = sorted(results.ado_results[3])
+    rwc_sum_data = sorted(results.ado_results[4])
+    rwc_mean_data = sorted(results.ado_results[5])
+    rwc_last_data = sorted(results.ado_results[6])
+
+    draw_data()
+
+    perform_statistical_analysis()
--- MuNeG/graph/analyser/StatisticalAnalysis.py
+++ MuNeG/graph/analyser/StatisticalAnalysis.py
@@ -10,8 +10,38 @@ def connect_to_data(host, database, user, password):
     curs = my_conn.cursor()
     return curs
 
-def draw_histogram(data):
-    fit = stats.norm.pdf(data, np.mean(data), np.std(data))
-    pl.plot(data,fit,'-o')
-    pl.hist(data, normed=True)
+def compare_distributions(a, b):
+    ks,pval = stats.ks_2samp(a,b)
+    print 'Kolgomorov-Smirnov: ' + str(ks) + ' p-value: ' + str(pval)
+
+    stat, pval = stats.mannwhitneyu(b, a)
+    print 'Mann-Whitney: ' + str(stat) + ' p-value: ' + str(pval)
+
+    stat, pval = stats.ttest_ind(b, a)
+    print '2-sided T-Test: ' + str(stat) + ' p-value: ' + str(pval)
+
+def fit_to_distribution(label, data):
+    print 'Stats for ' + label
+    mean = np.mean(data)
+    std = np.std(data)
+    median = np.median(data)
+    mode = stats.mode(data)
+    max = np.max(data)
+    stat, pval = stats.kstest(data, 'norm')
+    k2, norm_pval = stats.mstats.normaltest(data)
+
+    print 'Mean: ' + str(mean)
+    print 'Std: ' + str(std)
+    print 'Median: ' + str(median)
+    print 'Mode: ' + str(mode)
+    print 'Max: ' + str(max)
+    print 'Goodness of fit to norm: ' + str(stat) + ' p-value: ' + str(pval)
+    print 'DAgostino fit to norm: ' + str(k2) + ' p-value: ' + str(norm_pval)
+    return stats.norm.pdf(data, mean, std)
+
+def draw_plot(data_dict):
+    for data in data_dict.iteritems():
+        fit =  fit_to_distribution(data[0], data[1])
+        pl.plot(data[1], fit, label=data[0])
+    pl.legend(loc='upper right')
     pl.show()
